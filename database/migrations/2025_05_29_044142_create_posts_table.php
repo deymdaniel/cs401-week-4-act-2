fad44a27721049302fe4ad9d918a6e668c4c2e34
deymdaniel <danielbiacaniii@gmail.com>
2025-05-29 12:55:03 +0800

    Added table columns in migration

--- database/migrations/2025_05_29_044142_create_posts_table.php
+++ database/migrations/2025_05_29_044142_create_posts_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('Title')->comment('The title of the post');
+            $table->text('Content')->comment('The content of the post');
+            $table->String('Slug')->unique()->comment('The unique slug for the post');
+            $table->timestamp('Publication_Date')->nullable()->comment('The date and time when the post was published');
+            $table->timestamp("Last_modified_date")->nullable()->comment('The date and time when the post was last modified');
+            $table->string('Status')->comment('The status of the post (D - Draft, P - Published, I - Inactive)');
+            $table->string('Featured_image_url')->comment('The URL of the featured image for the post');
+            $table->integer('Views_count')->comment('The number of views the post has received')->default(0);
             $table->timestamps();
         });
     }
