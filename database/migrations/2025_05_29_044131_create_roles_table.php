50a010efc4c43a0d5ca9028e2f6834b650f42b62
deymdaniel <danielbiacaniii@gmail.com>
2025-05-29 12:58:12 +0800

    Added roles migration tables

--- database/migrations/2025_05_29_044131_create_roles_table.php
+++ database/migrations/2025_05_29_044131_create_roles_table.php
@@ -12,8 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('roles', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id()->comment('Primary key');
+            $table->string('role_name')->comment('Role name: Admin, Contributor, Subscriber');
+            $table->string('description')->comment('Description of the role');
+            $table->timestamps(); // Laravel created_at and updated_at
         });
     }
 
